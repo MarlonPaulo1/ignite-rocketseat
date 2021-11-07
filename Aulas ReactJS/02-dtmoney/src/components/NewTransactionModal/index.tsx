ed9baeec9e991ba21fd21c0d7646f7bf54a1777b
Marlon Paulo <marlonpaulo1@gmail.com>
2021-11-07 15:14:23 -0300

    Movendo criação para o context

--- Aulas ReactJS/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ Aulas ReactJS/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useContext, useState } from 'react'
 import Modal from 'react-modal'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import { api } from '../../services/api'
+import { TransactionsContext } from '../../TransactionsContext'
 
 interface NewTransactionModalProps {
     isOpen: boolean
@@ -12,6 +13,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+   const transactions = useContext(TransactionsContext)
+   
     const [title, setTitle] = useState('')
     const [value, setValue] = useState(0)
     const [category, setCategory] = useState('')
@@ -19,15 +22,6 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault()
-
-        const data ={
-            title,
-            value,
-            category,
-            type,
-        }
-
-        api.post('/transactions', data)
     }
    
     return (
